0c0b7c7e5c0673df09da0d06a0a3e43ce413f29b
OanhHy <nguyenthioanh.s186@gmail.com>
2025-03-03 22:17:41 +0700

    hihi

--- BTL_QuanLiKhachSan/UserControlForm/User_KhachHang.Designer.cs
+++ BTL_QuanLiKhachSan/UserControlForm/User_KhachHang.Designer.cs
@@ -29,24 +29,37 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(298, 284);
+            this.label1.Location = new System.Drawing.Point(335, 355);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(596, 251);
+            this.label1.Size = new System.Drawing.Size(670, 314);
             this.label1.TabIndex = 1;
             this.label1.Text = "Khách Hàng";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(514, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // User_KhachHang
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "User_KhachHang";
-            this.Size = new System.Drawing.Size(1193, 818);
+            this.Size = new System.Drawing.Size(1342, 1022);
             this.ResumeLayout(false);
 
         }
@@ -54,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- BTL_QuanLiKhachSan/UserControlForm/User_KhachHang.cs
+++ BTL_QuanLiKhachSan/UserControlForm/User_KhachHang.cs
@@ -16,5 +16,10 @@ namespace BTL_QuanLiKhachSan.UserControlForm
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello.");
+        }
     }
 }
